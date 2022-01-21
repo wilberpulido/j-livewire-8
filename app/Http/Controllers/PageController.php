a0cce2ca853477ab12f20c25a44f4cc20458cd76
wilberpulido <pulidowilber@gmail.com>
2022-01-20 23:37:51 -0300

    Subida del proyecto terminado del curso

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Course;
+
+class PageController extends Controller
+{
+    public function home()
+    {
+        return view('index');
+    }
+    public function course(Course $course)
+    {
+        return view('course',compact('course'));
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'free' => rand(0,1),
+
+            'course_id' => rand(1,10),
+        ];
+    }
+}
